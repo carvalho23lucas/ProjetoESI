8066824a6482668e5393095b8e339233dc293f57
Ingrid Machado Santos Garib <msg.ingrid@yahoo.com.br>
2016-10-17 22:54:27 +0000

    Welcome criado

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161016004842) do
+ActiveRecord::Schema.define(version: 20161016235136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,5 +42,19 @@ ActiveRecord::Schema.define(version: 20161016004842) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "usuarios", force: :cascade do |t|
+    t.string   "documento"
+    t.string   "nome"
+    t.string   "email"
+    t.string   "senha"
+    t.integer  "cidade_id"
+    t.boolean  "isPJ"
+    t.boolean  "isInstituicao"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["cidade_id"], name: "index_usuarios_on_cidade_id", using: :btree
+  end
+
   add_foreign_key "cidades", "estados"
+  add_foreign_key "usuarios", "cidades"
 end
