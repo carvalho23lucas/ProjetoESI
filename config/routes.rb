309f0e29a838ec72e2a8754cdba2893a5623b0c4
David Rodrigues <regis_2725@hotmail.com>
2016-10-20 04:07:07 +0000

    pagina padrao rodando(agora voces só precisam fazer a tag body), css pra header e footer, imagens, title, icon, algumas correções ficarão pendentes, mas nada que atrapalhe o funcionamento do sistema

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   resources :usuarios
   get 'home/index'
+  get 'cadastro/cadastrousuariodoador'
+  
   root 'home#index'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
