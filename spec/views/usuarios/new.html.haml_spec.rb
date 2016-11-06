7b4943f9f74e9f270607caa85fc40e35be5a8487
Lucas Carvalho <carvalho23lucas@gmail.com>
2016-11-06 22:49:11 +0000

    Atualizado spec de criação de usuários.

--- spec/views/usuarios/new.html.haml_spec.rb
+++ spec/views/usuarios/new.html.haml_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "usuarios/new", type: :view do
   before(:each) do
-    cidade = Cidade.new(:nome =>"Guarulhos", :estado => Estado.new(:sigla => "SP", :nome => "São Paulo"))
+    cidade = Cidade.new(:nome => "Guarulhos", :estado => Estado.new(:sigla => "SP", :nome => "São Paulo"))
+    @estados = [cidade.estado.nome, cidade.estado_id]
+    @cidades =  [cidade.nome, cidade.id]
     assign(:usuario, Usuario.new(
       :documento => "MyString",
       :nome => "MyString",
@@ -16,20 +18,13 @@ RSpec.describe "usuarios/new", type: :view do
 
   it "renders new usuario form" do
     render
-
     assert_select "form[action=?][method=?]", usuarios_path, "post" do
-
       assert_select "input#usuario_documento[name=?]", "usuario[documento]"
-
       assert_select "input#usuario_nome[name=?]", "usuario[nome]"
-
       assert_select "input#usuario_email[name=?]", "usuario[email]"
-
       assert_select "input#usuario_senha[name=?]", "usuario[senha]"
-
-      #assert_select "select#usuario_estado[name=?]", "cidade.estado"
-      
-      #assert_select "select#usuario_cidade[name=?]", "cidade.nome"
+      assert_select "select#estado[name=?]", "estado"
+      assert_select "select#usuario_cidade_id[name=?]", "usuario[cidade_id]"
 
     end
   end
