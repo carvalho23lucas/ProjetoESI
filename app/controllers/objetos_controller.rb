daa005bd6c2078d76a7fc95eaa87b236df367f47
Redher <redherwetzl@gmail.com>
2017-05-18 01:58:13 +0000

    again

--- app/controllers/objetos_controller.rb
+++ app/controllers/objetos_controller.rb
@@ -32,7 +32,7 @@ class ObjetosController < ApplicationController
     
     @objeto = Objeto.new(objeto_params)
     @categorias = CategoriaObjeto.all.order(:nome).map { |categoria| [categoria.nome, categoria.id]}.prepend(['Selecione uma categoria', 0])
-    @objeto.instituicao_id = ($instLogin.id = nil ? $instLogedin.id : 0)
+    @objeto.instituicao_id = $instLogedin.id
   
     respond_to do |format|
       if @objeto.save
