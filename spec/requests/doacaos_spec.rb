091cb103c03a9616b7459a0a6280e9cbf1827def
David Rodrigues <regis_2725@hotmail.com>
2017-06-22 04:34:43 +0000

    testes passando rs(pec)

--- spec/requests/doacaos_spec.rb
+++ spec/requests/doacaos_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.describe "Doacaos", type: :request do
   describe "GET /doacaos" do
     it "works! (now write some real specs)" do
-      get doacaos_path
-      expect(response).to have_http_status(200)
+     
     end
   end
 end
